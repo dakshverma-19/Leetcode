8338f50f9fbe4b78d11ffd00863d05df9f38ee69
Daksh Verma <dakshverma1912@gmail.com>
2026-08-12 01:53:43 +0530

    Time: 0 ms (100%), Space: 8.6 MB (21.12%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int reverse(int x) {
+        long long rev = 0;
+        
+       
+        while (x != 0) {
+            int pop = x % 10;
+            x /= 10;
+      
+            rev = rev * 10 + pop;
+            
+         
+            if (rev > INT_MAX || rev < INT_MIN) {
+                return 0;
+            }
+        }
+        
+        return static_cast<int>(rev);
+    }
+};    
