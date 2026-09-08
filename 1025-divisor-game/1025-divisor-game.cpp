54e4c2a51217274a644cd8da0228d55e264b15ac
Daksh Verma <dakshverma1912@gmail.com>
2026-09-08 23:10:26 +0530

    Time: 0 ms (100%), Space: 7.8 MB (58.72%) - LeetHub

--- 1025-divisor-game/1025-divisor-game.cpp
+++ 1025-divisor-game/1025-divisor-game.cpp
@@ -1,10 +1,9 @@
 class Solution {
 public:
     bool divisorGame(int n) {
-        if(n%2==0){
-            return true;
+        if(n%2==0)return true;
 
-        }
+        
         return false;
     }
 };
